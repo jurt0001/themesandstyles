262976e170ac641fe5d442e42b849831da0804c1
christianjurt <Longbeach@2>
2017-10-22 00:22:43 -0400

    second activity

---
+++ app/src/main/java/com/sriyanksiddhartha/stylesandthemesdemo/SecondActivity.java
@@ -0,0 +1,27 @@
+package com.sriyanksiddhartha.stylesandthemesdemo;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.transition.Explode;
+
+/**
+ * Created by christianjurt on 2017-10-21.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_second);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Explode explode = new Explode();
+            explode.setDuration(1000);
+            getWindow().setEnterTransition(explode);
+        }
+    }
+}
